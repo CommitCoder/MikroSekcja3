dd5d00e3921a27cab9c0d7765f8cf36f999a1591
KubaKraken <k.jakub.dev.007@gmail.com>
2021-04-25 15:08:29 +0200

    Video 11 @GetMapping @RequestMapping, simple example

---
+++ src/main/java/com/sekcja3/students/StudentController.java
@@ -0,0 +1,26 @@
+package com.sekcja3.students;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+
+
+    // @GetMapping     @RequestMapping  same effect, works the same,   @GetMapping better choice
+    @GetMapping("/hello")
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello(){
+        return "Witaj";
+    }
+
+
+
+
+
+
+}
